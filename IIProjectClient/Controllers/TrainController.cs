edbb29ad9e533d000f4819e0c700f177b1ccd305
jonas-g <jonas.gabrielson@me.com>
2017-03-09 14:09:16 +0100

    Service, Controller, View

---
+++ IIProjectClient/Controllers/TrainController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IIProjectClient.Controllers
+{
+    public class TrainController : Controller
+    {
+        // GET: Train
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Train/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Train/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Train/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Train/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Train/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Train/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Train/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
