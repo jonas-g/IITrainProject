406c9b9619f0c6d5a093aa8417cc6cfbc5a9bedb
jonas-g <jonas.gabrielson@me.com>
2017-03-09 15:00:28 +0100

    Foreign Keys?

--- IIProjectClient/Models/Fordon.cs
+++ IIProjectClient/Models/Fordon.cs
@@ -9,7 +9,7 @@ namespace IIProjectClient.Models
     {
         public string EPC { get; set; }
         public long EVN { get; set; }
-        public string Innehavare { get; set; }
+        public static string Innehavare { get; set; }
         public string UHAnsvarig { get; set; }
         public string Kategori { get; set; }
         public string Underkategori { get; set; }
--- IIProjectClient/Models/FordonsPassage.cs
+++ IIProjectClient/Models/FordonsPassage.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Xml.Linq;
 
 namespace IIProjectClient.Models
 {
     public class FordonsPassage
     {
-        public string FordonEPC { get; set; }
-        public string PlatsEPC { get; set; }
-        public DateTime PassageTid { get; set; }
+        public Fordon Fordonet { get; set; }
+        public Plats Platsen { get; set; }
+        public DateTime EventTid { get; set; }
 
+        public FordonsPassage()
+        {
+            XElement XXXXXXX = new XElement("", "");
+        }
     }
 }
