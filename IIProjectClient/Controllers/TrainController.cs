d96dd6595e7dd1864212e641cc68794387e1b880
jonas-g <jonas.gabrielson@me.com>
2017-03-10 12:40:33 +0100

    GetPassageInfo code

--- IIProjectClient/Controllers/TrainController.cs
+++ IIProjectClient/Controllers/TrainController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
 
 namespace IIProjectClient.Controllers
 {
     public class TrainController : Controller
     {
+        TrainServiceReference.TrainServiceClient localService = new TrainServiceReference.TrainServiceClient();
+
+        public TrainController()
+        {
+
+        }
+
         // GET: Train
         public ActionResult Index()
         {
+            
             return View();
         }
 
--- IIProjectService/TrainService.svc.cs
+++ IIProjectService/TrainService.svc.cs
@@ -20,14 +20,55 @@ namespace IIProjectService
         EpcisEventServiceClient epcisEventService = new EpcisEventServiceClient();
         NamingServiceClient namingService = new NamingServiceClient();
 
+        //public TrainService()
+        //{
+        //    DateTime start = new DateTime(2011, 03, 20);
+        //    DateTime stop = new DateTime(2011, 03, 28);
+        //    string loc = "urn:epc:id:sgln:735999271.000.13";
+        //    XElement svar = GetPassageInfo(start, stop, loc);
+        //}
+
         public XElement GetPassageInfo(DateTime fromDate, DateTime toDate, string epcLocation)
         {
             var eventsData = GetEvents(fromDate, toDate, epcLocation);
             var locationData = GetLocation(epcLocation);
-           // var vehicleData = Någont som via evensData hämtar vagnens id....
 
+            var query =
+                new XElement("Passages",
+                    from result in eventsData.Descendants("ObjectEvent")
+                    let vehicleEPC = result.Descendants("epc").FirstOrDefault().Value
+                    let locationEPC = result.Descendants("id").FirstOrDefault().Value
+                    let time = result.Descendants("eventTime").FirstOrDefault().Value
+                    let location = locationData.Descendants("Name").FirstOrDefault().Value
+                    let vehicle = GetVehicle(vehicleEPC)
+                    let vehicleEVN = vehicle.Descendants("Fordonsnummer").FirstOrDefault().Value
+                    let owner = vehicle.Descendants("Fordonsinnehavare").Elements("Foretag").FirstOrDefault().Value
+                    let maintenance = vehicle.Descendants("UnderhållsansvarigtForetag").Elements("Foretag").FirstOrDefault().Value
+                    let category = vehicle.Descendants("FordonskategoriKodFullVardeSE").FirstOrDefault().Value
+                    let subcategory = vehicle.Descendants("FordonsunderkategoriKodFullVardeSE").FirstOrDefault().Value
+                    //let authBool = 
+                    let authFromDate = vehicle.Descendants("Godkannande").FirstOrDefault().Element("GiltigtFrom").Value
+                    let authToDate = vehicle.Descendants("Godkannande").FirstOrDefault().Element("GiltigtTom").Value
+                    select
+                    new XElement("Passage",
+                        new XElement("VehicleEPC", vehicleEPC),
+                        new XElement("LocationEPC", locationEPC),
+                        new XElement("Time", time),
+                        new XElement("LocationName", location),
+                        new XElement("Vehicle",
+                            new XElement("EVN", vehicleEVN),
+                            new XElement("Owner", owner),
+                            new XElement("Maintenance", maintenance),
+                            new XElement("Category", category),
+                            new XElement("Subcategory", subcategory),
+                            //new XElement("Authorized", authBool),
+                            new XElement("AuthorizedFromDate", authFromDate),
+                            new XElement("AuthorizedToDate", authToDate)
+                            )
+                        )
+                    );
 
-            throw new NotImplementedException();
+            return query;
         }
 
 
