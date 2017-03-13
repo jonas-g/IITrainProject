7bc573f66d2753d12672f64c44797adc07eb5dc1
HermanBrodd <hmbrodd@gmail.com>
2017-03-13 13:19:09 +0100

    Ny klass för tjänstemeddelande

---
+++ IIProjectClient/Models/ServiceMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IIProjectClient.Models
+{
+    //Klass för tjänstemeddelande
+    public class ServiceMessage
+    {
+        public int AnswerCode { get; set; }
+        public string Message { get; set; }
+        public string ServiceManage { get; set; }
+        public string NameVersion { get; set; }
+        public DateTime AnswerTime { get; set; }
+        public string CallManager { get; set; }
+        public string SentArgument { get; set; }
+    }
+}
