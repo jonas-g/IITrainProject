003aa5a27972d3fc189e5ed7ee9f2aa2c089159e
Mayflya <albin.lundberg@gmail.com>
2017-03-15 12:05:23 +0100

    Added fromXMl method in model

--- IIProjectClient/Models/VehiclePassage.cs
+++ IIProjectClient/Models/VehiclePassage.cs
@@ -14,10 +14,32 @@ namespace IIProjectClient.Models
 
         public VehiclePassage()
        
-        //Magnus testar GIT!!
-        
         {
             XElement XXXXXXX = new XElement("", "");
+        }
+
+        public static void fromXML(XElement xmlResponse)
+        {
+            VehiclePassage vehiclePassage = new VehiclePassage();
+            Location location = new Location();
+            Vehicle vehicle = new Vehicle();
+
+            vehiclePassage.EventTime = DateTime.Now;// String behöver göras till datetime xmlResponse.Element("Time").Value;
+
+            location.Name = xmlResponse.Element("LocationName").Value;
+            location.EPC = xmlResponse.Element("LocationEPC").Value;
+
+            var tempQuery = xmlResponse.Element("Vehicle");
+
+            vehicle.EPC = xmlResponse.Element("VehicleEPC").Value;
+            vehicle.EVN =Int32.Parse( tempQuery.Element("EVN").Value);
+            vehicle.Owner = tempQuery.Element("Owner").Value;
+            vehicle.Maintenance = tempQuery.Element("Maintenance").Value;
+            vehicle.Category = tempQuery.Element("Category").Value;
+            vehicle.Subcategory = tempQuery.Element("Subcategory").Value;
+            //evetuell info om auth(godkännande)
+
+
         }
     }
 }
